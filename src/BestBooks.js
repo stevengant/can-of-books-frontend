6fe3ce5ba328afcb3957037e26e48132d09e3d39
stevengant <sgant39@gmail.com>
2023-01-24 02:17:26 +0000

    frontend complete

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,19 +11,62 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`;
+
+      let bookData = await axios.get(url);
+      console.log(bookData);
+      this.setState({
+        books: bookData.data
+      });
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
 
   render() {
+    console.log(this.state.books);
 
     /* TODO: render all the books in a Carousel */
-
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <Carousel>
+            {this.state.books.map((bookElem, idx) => {
+                return(
+                  <Carousel.Item key={idx}>
+                  <img
+                    className='d-block w-100'
+                    src={bookElem.img_URL}
+                    alt={bookElem.description}
+                  />
+                  <Carousel.Caption>
+                    <h3 style={{
+                      backgroundColor: 'grey',
+                      borderRadius: '5px',
+                      width: 'max-content',
+                      margin: 'auto',
+                      padding: '5px'
+                    }}
+                    >
+                      {bookElem.title}
+                    </h3>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                );
+
+            })};
+          </Carousel>
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>🙀 No Books Found 🙀</h3>
         )}
       </>
     )
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import './Header.css';
 
 class Header extends React.Component {
   render() {
@@ -8,7 +9,7 @@ class Header extends React.Component {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        <NavItem><Link to="/About" className="nav-link">About Us</Link></NavItem>
       </Navbar>
     )
   }
